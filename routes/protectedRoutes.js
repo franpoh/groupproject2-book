f7176e913c67a0ff378273d54f16f91515415761
AuntPyoneMaung <za7196@gmail.com>
2022-01-13 11:08:18 +0800

    minor edit of reviewService

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -12,7 +12,7 @@ const grabController = new GrabController();
 router.post("/protected/:indexId/addReview", reviewController.addReview);
 
 // for grabbing book with credit
-router.post('/protected/swap', authenticateJwt, grabController.grabBook);
+router.post('/protected/swap', grabController.grabBook);
 
 
 // G1 100122: for testing only. Also query some real life examples use session unique URIs, is it due prevent bookmark/copied URL + cached data + long expiry? eg . /e6xxh61s/swap
