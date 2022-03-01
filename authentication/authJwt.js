b61de9b7c371b38310886c20b2e0bda100a209ce
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-01 18:27:14 +0800

    redoing authentication

--- authentication/authJwt.js
+++ authentication/authJwt.js
@@ -5,37 +5,22 @@ const jwt = require("jsonwebtoken"); // Import
 module.exports = function (req, res, next) {
     const { accessToken, refreshToken } = req.cookies;
 
-    // const authHeader = req.headers['authorization']; // Bearer <token>
+    // if (accessToken == null) {
+    //     res.status(401);
+    //     return res.send("Please login or sign up!");
+    // }
 
-    // console.log("authJwt", authHeader);
-
-    // // if we have an authHeader, then return the token portion of authHeader, otherwise return undefined
-    // const token = authHeader && authHeader.split(' ')[1]; // splitting Bearer from <token>
-
-    if (accessToken == null) {
-        res.status(401);
-        return res.send("Please login or sign up!");
-    }
-
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, function (err, user) {
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-            if (refreshToken == null) {
-                res.status(401);
-                return res.send("Please login or sign up!");
-            } else {
-                jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, function (err, user) {
-                    if (err) {
-                        res.status(403); // token no longer valid
-                        res.send("Please login again.")
-                    }
-            
-                    const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
-                    res.cookie('accessToken', accessToken, { httpOnly: true }); 
-                });
-            }
-
-            res.status(403); // token no longer valid
-            res.send("Please login again.")
+            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) {
+                    res.status(403); // token no longer valid
+                    res.send("Please login again.")
+                }
+        
+                const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+                res.cookie('accessToken', accessToken, { httpOnly: true }); 
+            });
         }
 
         req.username = user.username; // eg cmdrshep
