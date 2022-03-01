749b508bb0498290ec4546ddd1cb06bc72ebe7af
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-01 18:48:09 +0800

    redoing authentication

--- authentication/authJwt.js
+++ authentication/authJwt.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const jwt = require("jsonwebtoken"); // Import
 
 module.exports = function (req, res, next) {
+    console.log("authJwt 1")
     const { accessToken, refreshToken } = req.cookies;
 
     // if (accessToken == null) {
@@ -12,6 +13,7 @@ module.exports = function (req, res, next) {
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
+            console.log("authJwt 2")
             jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
                 if (err) {
                     res.status(403); // token no longer valid
@@ -20,6 +22,7 @@ module.exports = function (req, res, next) {
         
                 const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
                 res.cookie('accessToken', accessToken, { httpOnly: true }); 
+                console.log("authJwt 3")
             });
         }
 
@@ -27,7 +30,7 @@ module.exports = function (req, res, next) {
         req.userId = user.userId // eg 5
 
         console.log("authJwt", req.username, req.userId)
-
+        console.log("authJwt 4")
         next();
     });
 }
