238e28e3bf9f3010cbe3e1652bc9d5f31a4397be
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-02 22:25:56 +0800

    redoing login/register output

--- services/loginService.js
+++ services/loginService.js
@@ -13,9 +13,6 @@ module.exports = {
             data: null,
         }
 
-        // const user = await Users.findOne({ where: { email: email } });
-        // const passwordVerification = await bcrypt.compare(password, user.password);
-
         let p = new Promise((resolve, reject) => {
             const user = await Users.findOne({ where: { email: email } });
             const passwordVerification = await bcrypt.compare(password, user.password);
@@ -50,45 +47,8 @@ module.exports = {
             return result;
         })
 
-        // let pPwd = new Promise((resolve, reject) => {
-        //     const passwordVerification = await bcrypt.compare(password, user.password);
-        //     if (passwordVerification) {
-        //         resolve(passwordVerification)
-        //     } else if (!passwordVerification) {
-        //         reject("You have entered the wrong password")
-        //     }
-        // });
-
-
-
-        // if (!user) {
-        //     result.message = "User not found. You have entered the wrong email, please try logging in again.";
-        //     result.status = 400;
-        //     return result;
-        // }
-
-        // if (!passwordVerification) {
-        //     result.message = "You have entered the wrong password";
-        //     result.status = 400;
-        //     return result;
-        // }
-
-        // if (user && passwordVerification) {
-        //     const loginData = {
-        //         userId: user.userId,
-        //         username: user.username
-        //     }
-
-        //     const accessToken = jwt.sign(loginData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-        //     const refreshToken = jwt.sign(loginData, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
-
-        //     result.data = {
-        //         accessToken: accessToken,
-        //         refreshToken: refreshToken,
-        //     };
-        //     result.status = 200;
-        //     result.message = "Your login is successful!";
-        //     return result;
-        // }
+        result.status = 200;
+        result.message = "Your login is successful!";
+        return result;
     }
 }
