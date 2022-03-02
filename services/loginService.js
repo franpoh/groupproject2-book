c5e05142c92eb08f82da9ba53985e4d7e1e39fd6
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-02 20:24:01 +0800

    redoing login/register output

--- services/loginService.js
+++ services/loginService.js
@@ -21,56 +21,28 @@ module.exports = {
         //     return result;
         // }
 
-        // const passwordVerification = await bcrypt.compare(password, user.password);
+        const passwordVerification = await bcrypt.compare(password, user.password);
 
-        // if (!passwordVerification) {
-        //     result.message = "You have entered the wrong password";
-        //     result.status = 400;
-        //     return result;
-        // }
-
-        // const loginData = {
-        //     userId: user.userId,
-        //     username: user.username
-        // }
-
-        // const accessToken = jwt.sign(loginData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-        // const refreshToken = jwt.sign(loginData, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
-
-        // result.data = {
-        //     accessToken: accessToken, 
-        //     refreshToken: refreshToken, 
-        // };
-        // result.status = 200;
-        // result.message = "Your login is successful!";
-        // return result;
-
-        try {
-            const user = await Users.findOne({ where: { email: email } });
-
-            if (user) {
-                const passwordVerification = await bcrypt.compare(password, user.password);
-            }
-
-            const loginData = {
-                userId: user.userId,
-                username: user.username
-            }
-
-            const accessToken = jwt.sign(loginData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-            const refreshToken = jwt.sign(loginData, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
-
-            result.data = {
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-            };
-            result.status = 200;
-            result.message = "Your login is successful!";
-            return result;
-        } catch (error) {
-            result.message = "There was an error";
+        if (!passwordVerification) {
+            result.message = "You have entered the wrong password";
             result.status = 400;
             return result;
         }
+
+        const loginData = {
+            userId: user.userId,
+            username: user.username
+        }
+
+        const accessToken = jwt.sign(loginData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+        const refreshToken = jwt.sign(loginData, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+
+        result.data = {
+            accessToken: accessToken, 
+            refreshToken: refreshToken, 
+        };
+        result.status = 200;
+        result.message = "Your login is successful!";
+        return result;
     }
 }
