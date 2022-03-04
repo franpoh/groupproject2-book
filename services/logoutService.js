c5a4050b46c8dca55e721508441021d071473313
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 19:04:11 +0800

    create logout service

--- services/logoutService.js
+++ services/logoutService.js
@@ -7,9 +7,11 @@ module.exports = {
         }
 
         try {
+            const { accessToken, refreshToken } = req.cookies;
+
             console.log("TESTING LOGOUT");
-            res.clearCookie('refreshToken');
-            res.clearCookie('accessToken');
+            res.clearCookie('refreshToken', refreshToken, { httpOnly: true, sameSite: "None", secure: true });
+            res.clearCookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
             
             result.status = 204;
             result.message = "Your logout is successful!";
