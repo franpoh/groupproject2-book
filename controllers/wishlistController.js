b2d3cbe1c79bda772c6e626478381deb856178d7
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-04 11:20:37 +0800

    Update wishlistController.js

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -111,7 +111,7 @@ class WishlistController {
 
         // console.log('checkMyWishlist Controller', loginId, req.body.userId); // body should be empty for checkMyWishlist but amended to use body.userId due webapp etc etc
 
-        const test = parseInt(req.body.userId)
+        // const test = parseInt(req.body.userId)
 
         // const result = await wishlistSerivce.checkMyWishlist(loginId, req.body.userId);
         const result = await wishlistSerivce.checkMyWishlist(loginId);
