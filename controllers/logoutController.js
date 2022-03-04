a0632c359ff3fb7eda16632af395541a5214a08a
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 23:20:10 +0800

    create logout service

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,20 +1,22 @@
-const logoutService = require("../services/logoutService");
-
 class logoutController {
     async logout(req, res) {
-        // try {
-        //     const { accessToken, refreshToken } = req.cookies;
+        let status = "";
+        let msg = "";
 
-        //     res.clearCookie('refreshToken', refreshToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
-        //     res.clearCookie('accessToken', accessToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
+        try {
+            const { accessToken, refreshToken } = req.cookies;
+
+            res.clearCookie('refreshToken', refreshToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
+            res.clearCookie('accessToken', accessToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
             
-        //     return res.status(204).json({ message: "Your logout is successful!" });
-        // } catch (err) {
-        //     return res.status(400).json({ message: "Your logout is unsuccessful!" });
-        // }
+            status = 204;
+            msg = "Your logout is successful!"
+        } catch (err) {
+            status = 400;
+            msg = "Your logout is unsuccessful!"
+        }
 
-        const result = await logoutService.logout(req.cookies);
-        return res.status(result.status).json({ message: result.message });
+        return res.status(status).json({ message: msg });
     }
 }
 
