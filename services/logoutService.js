7275ee3442027d60b65a620b63f6689dd6dafdd4
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 19:51:50 +0800

    create logout service

--- services/logoutService.js
+++ services/logoutService.js
@@ -10,8 +10,8 @@ module.exports = {
             const { accessToken, refreshToken } = req.cookies;
 
             console.log("TESTING LOGOUT");
-            res.clearCookie('refreshToken', {domain: "https://book-libraryshop.herokuapp.com", path: "/protected/logout"});
-            res.clearCookie('accessToken', {domain: "https://book-libraryshop.herokuapp.com", path: "/protected/logout"});
+            res.clearCookie('refreshToken', {domain: "book-libraryshop.herokuapp.com", path: "/"});
+            res.clearCookie('accessToken', {domain: "book-libraryshop.herokuapp.com", path: "/"});
             
             result.status = 204;
             result.message = "Your logout is successful!";
