4399c0141478a5d2623e94c81d2f73580cee5ace
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 23:00:16 +0800

    create logout service

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,45 +1,17 @@
 class logoutController {
     async logout(req, res) {
-        let result = {
-            message: null,
-            status: null,
-            data: null,
-        }
+        try {
+            const { accessToken, refreshToken } = req.cookies;
 
-        // try {
-        //     const { accessToken, refreshToken } = req.cookies;
+            res.clearCookie('refreshToken', refreshToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
+            res.clearCookie('accessToken', accessToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
 
-        //     res.clearCookie('refreshToken', refreshToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
-        //     res.clearCookie('accessToken', accessToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
+            console.log("Checking for Cookies: ", req.cookies);
             
-        //     return res.status(204).json({ message: "Your logout is successful!" });
-        // } catch (err) {
-        //     return res.status(400).json({ message: "Your logout is unsuccessful!" });
-        // }
-
-        let p = await new Promise ((resolve, reject) => {
-            // const { accessToken, refreshToken } = req.cookies;
-
-            res.cookie('refreshToken', "rejected");
-            res.cookie('accessToken', "rejected");
-
-            console.log("Checking for cookies: ", req.cookies)
-
-            resolve("Your logout is successful!");
-            reject("Your logout is unsuccessful!");
-        })
-
-        p.then((msg) => {
-            result.message = msg;
-            result.status = 204;
-            return result;
-        }).catch((err) => {
-            result.message = err;
-            result.status = 400;
-            return result;
-        })
-
-        return res.status(result.status).json({ message: result.message });
+            return res.status(204).json({ message: "Your logout is successful!" });
+        } catch (err) {
+            return res.status(400).json({ message: "Your logout is unsuccessful!" });
+        }
     }
 }
 
