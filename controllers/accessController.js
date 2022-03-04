435c441572839f5a899bda2fbaad25cb44cf0a17
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 21:54:07 +0800

    create logout service

--- controllers/accessController.js
+++ controllers/accessController.js
@@ -28,8 +28,8 @@ class accessController {
         const result = await loginService.login(req.body.email.toString(), req.body.password.toString());
 
         if (result.status == 200) {
-            res.cookie('refreshToken', result.data.refreshToken, { httpOnly: true, sameSite: "None", secure: true });
-            res.cookie('accessToken', result.data.accessToken, { httpOnly: true, sameSite: "None", secure: true });
+            res.cookie('refreshToken', result.data.refreshToken, { path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true });
+            res.cookie('accessToken', result.data.accessToken, { path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true });
             return res.status(result.status).json({ message: result.message });
         } else if (result.status == 400) {
             return res.status(result.status).json({ message: result.message });
