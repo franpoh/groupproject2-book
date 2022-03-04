ab455ff27abeb5c8ba3c9cd6e2341811aba7a262
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-04 09:00:09 +0800

    Update wishlistController.js

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -92,9 +92,10 @@ class WishlistController {
         
         res.status(200);
         return res.json({
-            data: {
+            data: { data: {
                 sendIndexID: sendIndexID,
-                indexId: req.body.indexId
+                indexId: (req.body.indexId)
+                }
             },
             message: 'result.message '
         });
