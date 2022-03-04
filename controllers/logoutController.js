1950487cd17cca72ca7479ddacb52914b28c2fbe
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-05 00:33:37 +0800

    create logout service

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -4,8 +4,8 @@ class logoutController {
         try {
             const { accessToken, refreshToken } = req.cookies;
 
-            res.clearCookie('refreshToken', {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
-            res.clearCookie('accessToken', {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
+            res.clearCookie('refreshToken', {httpOnly: true, sameSite: "None", secure: true});
+            res.clearCookie('accessToken', {httpOnly: true, sameSite: "None", secure: true});
             
             res.status(200);
             return res.json({ message: "Your logout is successful!" });
