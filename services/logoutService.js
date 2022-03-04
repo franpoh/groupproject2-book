60c423ad376d34ba24313e9122a979da4b640f3e
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 21:04:18 +0800

    create logout service

--- services/logoutService.js
+++ services/logoutService.js
@@ -6,10 +6,11 @@ module.exports = {
             data: null,
         }
 
+        const { accessToken, refreshToken } = req.cookies;
+
         try {
             console.log("TESTING LOGOUT", req.cookies);
 
-            const { accessToken, refreshToken } = req.cookies;
             res.clearCookie('refreshToken', refreshToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
             res.clearCookie('accessToken', accessToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
             
