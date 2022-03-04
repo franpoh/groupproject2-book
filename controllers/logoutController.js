155aa5dcadcf67a8055e4e076dcae990596d1d7e
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 21:47:05 +0800

    create logout service

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -2,7 +2,6 @@ const logoutService = require("../services/logoutService");
 
 class logoutController {
     async logout(req, res) {
-        // const result = await logoutService.logout(req.cookies);
         try {
             const { accessToken, refreshToken } = req.cookies;
 
@@ -13,8 +12,6 @@ class logoutController {
         } catch (err) {
             return res.status(400).json({ message: "Your logout is unsuccessful!" });
         }
-
-        // return res.status(result.status).json({ data: result.data, message: result.message });
     }
 }
 
