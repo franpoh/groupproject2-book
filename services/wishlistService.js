1b19b9055c4ad1a0ceed9eefe4ecbf72dac63c0e
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-04 10:50:34 +0800

    Update wishlistService.js

--- services/wishlistService.js
+++ services/wishlistService.js
@@ -181,7 +181,7 @@ module.exports = {
         const swapRelatedWish = await Swap.findAll({
             where: {
                 // indexId: submittedIndexId,
-                indexId: 7
+                indexId: [5,7]
                 // availability: 'YES'
             }
         }); // this format might not be useful.. might need nested array loop to tie to individual indexId
