0702be9f1460fb85447b6cba34fd0f68bb4e7e83
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-04 23:24:01 +0800

    create logout service

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,7 +1,5 @@
 class logoutController {
     async logout(req, res) {
-        let status = "";
-        let msg = "";
 
         try {
             const { accessToken, refreshToken } = req.cookies;
@@ -9,14 +7,12 @@ class logoutController {
             res.clearCookie('refreshToken', refreshToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
             res.clearCookie('accessToken', accessToken, {path: "/", domain: "book-libraryshop.herokuapp.com", httpOnly: true, sameSite: "None", secure: true});
             
-            status = 204;
-            msg = "Your logout is successful!"
+            res.status(204);
+            return res.json({ message: "Your logout is successful!" });
         } catch (err) {
-            status = 400;
-            msg = "Your logout is unsuccessful!"
+            res.status(400);
+            return res.json({ message: "Your logout is unsuccessful!" });
         }
-
-        return res.status(status).json({ message: msg });
     }
 }
 
