4459a637ce116a5fe5dcc4992bdf01429769c1a5
AuntPyoneMaung <za7196@gmail.com>
2022-03-06 00:48:05 +0800

    x

--- services/searchService.js
+++ services/searchService.js
@@ -138,7 +138,7 @@ module.exports = {
             }
         })
 
-        if (!byIndex) {
+        if (!paramsId) {
             result.data = review;
             result.message = `reviews retrieved`;
             result.status = 200;
