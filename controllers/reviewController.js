27b5fa83eedf754649f010f13b54b4482dd92651
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-06 22:05:29 +0800

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -13,34 +13,31 @@ class reviewController {
         };
 
         const loginId = req.userId; //session token
-        const receivedIndexId = req.body.indexId;
+        const receivedIndexId = parseInt(req.body.indexId);
         const receivedRev = req.body.rev;
         console.log(typeof receivedIndexId, typeof receivedRev)
 
-        // if (
-        //     typeof req.params.indexId !== "string" ||
-        //     typeof req.body.rev !== "string" ||
-        //     typeof req.body.userId !== "number"
-        // ) {
-        //     res.status(400);
-        //     return res.json({ message: "Incorrect request data" });
-        // };      
+        if (
+            typeof receivedIndexId !== "number" ||
+            typeof receivedRev !== "string"
+        ) {
+            res.status(400);
+            return res.json({ message: "Incorrect request data" });
+        };      
 
-        // const result = await reviewService.addReview(req.body.userId, req.params.indexId, req.body.rev);
-        // res.status(result.status);
-        // return res.json({ data: result.data, message: result.message });
+        const result = await reviewService.addReview(loginId, receivedIndexId, receivedRev);
+        res.status(result.status);
+        return res.json({ data: result.data, message: result.message });
 
         // testing
-        res.status(200);        
-        // const result = await reviewService.addReview(req.body.userId, req.params.indexId, req.body.rev);
-        
-        return res.json({
-            data: {
-                paramIndex: typeof receivedIndexId,
-                revType: typeof receivedRev
-            },
-            message: 'testing the type of data received'
-        });
+        // res.status(200);        
+        // return res.json({
+        //     data: {
+        //         paramIndex: typeof receivedIndexId,
+        //         revType: typeof receivedRev
+        //     },
+        //     message: 'testing the type of data received'
+        // });
         // testing
     };
 
