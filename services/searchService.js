68304f01de0ae5cc7c255b2c2dd1e41b97a2ddde
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-06 14:22:21 +0800

    Update searchService.js

--- services/searchService.js
+++ services/searchService.js
@@ -134,9 +134,10 @@ module.exports = {
 
                 for(xx=0; xx < swapForIndex.length; xx++){
                     const matchUserName = await Users.findByPk(swapForIndex[xx].userId);
-                    // (swapForIndex[xx])['username'] = matchUserName.username;
-                    testArray.push({...swapForIndex[xx], username: matchUserName.username});
+                    // (swapForIndex[xx])['username'] = matchUserName.username;                    
                     // testArray[xx] = { data: testArray[xx], username: matchUserName.username };
+                    testArray[xx] = { data: testArray[xx] };
+                    testArray[xx].data.username = matchUserName.username;
                     
                 };
 
