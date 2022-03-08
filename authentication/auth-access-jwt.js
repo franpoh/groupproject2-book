2b1f3fb12d4ae138cb718dd8e860053d84aca3d7
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-09 03:20:03 +0800

    redoing purchase history + associated foreign key

--- authentication/auth-access-jwt.js
+++ authentication/auth-access-jwt.js
@@ -3,24 +3,10 @@ require('dotenv').config();
 const jwt = require("jsonwebtoken"); // Import
 
 module.exports = function (req, res, next) {
-    const { accessToken, refreshToken } = req.cookies;
+    const { accessToken } = req.cookies;
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-            // jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-            //     if (err) {
-            //         res.status(403); // token no longer valid
-            //         res.send("Please login again.")
-            //     } else {
-            //         const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-            //         res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
-
-            //         req.username = user.username;
-            //         req.userId = user.userId;
-
-            //         next();
-            //     }
-            // });
             next();
         } else {
             req.username = user.username;
--- authentication/auth-refresh-jwt.js
+++ authentication/auth-refresh-jwt.js
@@ -7,20 +7,21 @@ module.exports = function (req, res, next) {
 
     if (req.username && req.userId) {
         next();
-    } else {
-        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-            if (err) {
-                console.log("Refresh Token Authentication Error: ", err)
-                res.status(403).json({ message: "Please login again." }); // token no longer valid
-            } else if (user) {
-                const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-                res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
-
-                req.username = user.username;
-                req.userId = user.userId;
-
-                next();
-            }
-        });
     }
+    
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            console.log("Refresh Token Authentication Error: ", err)
+            res.status(403).json({ message: "Please login again." }); // token no longer valid
+        } else if (user) {
+            const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+            res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
+
+            req.username = user.username;
+            req.userId = user.userId;
+
+            next();
+        }
+    });
+
 }
