195c47b632dc57e73e4b9e9bce4dc33d99a9fa47
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-09 03:01:14 +0800

    redoing purchase history + associated foreign key

--- services/view-profile-service.js
+++ services/view-profile-service.js
@@ -17,10 +17,7 @@ module.exports = {
         }
 
         const reviews = await Reviews.findAll({ where: { userId: userId }, include: "Index" });
-        // const swap = await Swap.findAll({ where: { userId: userId }, include: "Index" });
-        // const purchaseHistory = await Swap.findAll({ where: { purchasedId: userId }, include: "Index" });
-        // const swap = await Swap.findAll({ where: { userId: userId }, include: "Index" });
-        const swap = [];
+        const swap = await Swap.findAll({ where: { userId: userId }, include: "Index" });
         const purchaseHistory = await Swap.findAll({ where: { purchasedId: userId }, include: "Index" });
 
         result.data = {
