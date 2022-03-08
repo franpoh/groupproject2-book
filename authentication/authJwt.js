fc9ef462a12ac318aa5ec31123ea2a958e1d50b0
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-08 13:01:22 +0800

    refactored authentication to remove nesting

--- authentication/authJwt.js
+++ authentication/authJwt.js
@@ -7,24 +7,21 @@ module.exports = function (req, res, next) {
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-            console.log("Access token authentication error: ", err);
+            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) {
+                    res.status(403); // token no longer valid
+                    res.send("Please login again.")
+                } else {
+                    const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+                    res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
+
+                    req.username = user.username;
+                    req.userId = user.userId;
+
+                    next();
+                }
+            });
         } else {
-            req.username = user.username;
-            req.userId = user.userId;
-
-            next();
-        }
-    });
-
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            console.log("Refresh token authentication error: ", err);
-            res.status(403); // token no longer valid
-            res.send("Please login again.")
-        } else {
-            const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-            res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
-
             req.username = user.username;
             req.userId = user.userId;
 
