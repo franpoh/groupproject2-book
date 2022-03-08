06cbea192f8cad36988f1e3dba9fa0ea64260bae
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-08 12:49:42 +0800

    refactored authentication to remove nesting

--- authentication/authJwt.js
+++ authentication/authJwt.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const refreshJwt = require("./auth-refresh-jwt");
 
 const jwt = require("jsonwebtoken"); // Import
 
@@ -6,22 +7,23 @@ module.exports = function (req, res, next) {
     const { accessToken, refreshToken } = req.cookies;
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (user) {
+            req.username = user.username;
+            req.userId = user.userId;
+            next();
+        } else if (err) {
+            console.log("Access Token Authentication Error: ", err)
+        }
+    });
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) {
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-                if (err) {
-                    res.status(403); // token no longer valid
-                    res.send("Please login again.")
-                } else {
-                    const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-                    res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
+            console.log("Refresh Token Authentication Error: ", err)
+            res.status(403).json({ message: "Please login again." }); // token no longer valid
+        } else if (user) {
+            const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+            res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
 
-                    req.username = user.username;
-                    req.userId = user.userId;
-
-                    next();
-                }
-            });
-        } else {
             req.username = user.username;
             req.userId = user.userId;
 
