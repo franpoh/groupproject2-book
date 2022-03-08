0090280942b54796ee2ec4182283c85555e12365
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-08 12:40:37 +0800

    refactored authentication to remove nesting

--- authentication/authJwt.js
+++ authentication/authJwt.js
@@ -1,30 +1,21 @@
 require('dotenv').config();
+const refreshJwt = require("./auth-refresh-jwt");
 
 const jwt = require("jsonwebtoken"); // Import
 
 module.exports = function (req, res, next) {
-    const { accessToken, refreshToken } = req.cookies;
+    const { accessToken } = req.cookies;
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-                if (err) {
-                    res.status(403).json({ message: "Please login again." }); // token no longer valid
-                } else {
-                    const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
-                    res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
-
-                    req.username = user.username;
-                    req.userId = user.userId;
-
-                    next();
-                }
-            });
-        } else {
+        if (user) {
             req.username = user.username;
             req.userId = user.userId;
-
             next();
+        } else if (err) {
+            console.log("Access Token Authentication Error: ", err)
+            return;
         }
     });
+
+    refreshJwt();
 }
