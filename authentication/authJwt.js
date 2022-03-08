330181d0c0f78287d8a8c231b6b09f01cbf0743d
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-08 12:52:19 +0800

    refactored authentication to remove nesting

--- authentication/authJwt.js
+++ authentication/authJwt.js
@@ -11,16 +11,13 @@ module.exports = function (req, res, next) {
             req.username = user.username;
             req.userId = user.userId;
             next();
-        } else if (err) {
+        } else {
             console.log("Access Token Authentication Error: ", err)
         }
     });
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            console.log("Refresh Token Authentication Error: ", err)
-            res.status(403).json({ message: "Please login again." }); // token no longer valid
-        } else if (user) {
+        if (user) {
             const accessToken = jwt.sign({ userId: user.userId, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
             res.cookie('accessToken', accessToken, { httpOnly: true, sameSite: "None", secure: true });
 
@@ -28,6 +25,9 @@ module.exports = function (req, res, next) {
             req.userId = user.userId;
 
             next();
+        } else {
+            console.log("Refresh Token Authentication Error: ", err)
+            res.status(403).json({ message: "Please login again." }); // token no longer valid
         }
     });
 }
