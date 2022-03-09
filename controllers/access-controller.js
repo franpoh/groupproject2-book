d99ef52d53c592afb11aa24301361628d1db497f
franpoh <91236694+franpoh@users.noreply.github.com>
2022-03-09 22:30:20 +0800

    reformatted error catching and added new middleware for error catching

--- controllers/access-controller.js
+++ controllers/access-controller.js
@@ -1,37 +1,60 @@
 const registerService = require("../services/register-service.js");
 const loginService = require("../services/login-service.js");
 const Constants = require("../constants/index");
+const { errorCatch, validEmail, pwdByteLen } = require("../constants/error-catch"),
 
 class accessController {
     async register(req, res) {
+
         // check if password used is longer than 72 bytes long
         // bcrypt 5.0.1 limitations, only 1st 72 bytes of string are used in hashing
-        let checkLength = new TextEncoder().encode(req.body.password).length;
-        console.log("Checking byte length: ", checkLength);
+        let checkLength = pwdByteLen(req.body.password);
 
-        if (!req.body.password || checkLength > 72 || req.body.password.length < 5) {
-            return res.status(400).json({ message: "Password should contain at least 5 chars." });
-        }
+        // check if email meets all email formatting requirements
+        let vEmail = validEmail(req.body.email);
+        
+        // error checking
+        errorCatch(!vEmail, Constants.EMAIL_INVALID);
+        errorCatch(!req.body.password || !req.body.username || !req.body.email, Constants.GENERAL_INVALID);
+        errorCatch(checkLength > 72 || req.body.password.length < 5, Constants.PASSWORD_CHARS);
+        errorCatch(req.body.username.length < 3 || req.body.username.length > 10, Constants.USER_CHARS);
+
+        // if (!req.body.password || !req.body.username || !req.body.email) {
+        //     return res.status(400).json({ message: Constants.GENERAL_INVALID });
+        // }
+
+        // if (checkLength > 72 || req.body.password.length < 5) {
+        //     return res.status(400).json({ message: Constants.PASSWORD_CHARS });
+        // }
+
+        // if (req.body.username.length < 3 || req.body.username.length > 10) {
+        //     return res.status(400).json({ message: Constants.USER_CHARS });
+        // }
 
         const result = await registerService.register(req.body.email.toString(), req.body.username.toString(), req.body.password.toString());
+        
         return res.status(result.status).json({ message: result.message });
     }
 
     async login(req, res) {
+
+        // error catching
         if (!req.body.email) {
-            return res.status(400).json({ message: "Your email is invalid." });
+            return res.status(400).json({ message: Constants.EMAIL_INVALID });
         }
 
         if (!req.body.password) {
-            return res.status(400).json({ message: "Your password is invalid." });
+            return res.status(400).json({ message: Constants.PASSWORD_INVALID });
         }
 
         const result = await loginService.login(req.body.email.toString(), req.body.password.toString());
 
         if (result.status == 200) {
+            // Inserting cookies for access and refresh token 
             res.cookie(Constants.REFRESH_TOKEN, result.data.refreshToken, { httpOnly: true, sameSite: "None", secure: true });
             res.cookie(Constants.ACCESS_TOKEN, result.data.accessToken, { httpOnly: true, sameSite: "None", secure: true });
             return res.status(result.status).json({ message: result.message });
+
         } else if (result.status == 400) {
             return res.status(result.status).json({ message: result.message });
         }
--- controllers/account-controller.js
+++ controllers/account-controller.js
@@ -1,30 +1,47 @@
 const viewProfileService = require("../services/view-profile-service");
 const editProfileService = require("../services/edit-profile-service")
+const Constants = require("../constants/index");
+const { errorCatch, validEmail, pwdByteLen } = require("../constants/error-catch"),
 
 class accountController {
     async viewProfile(req, res) {
+
         const result = await viewProfileService.viewProfile(req.userId); // using values passed from authenticateJwt
-        res.status(result.status);
-        return res.json({ data: result.data, message: result.message });
+       
+        return res.status(result.status).json({ message: result.message });
     }
 
     async editProfile(req, res) {
-        let checkLength = new TextEncoder().encode(req.body.newPassword).length;
-        console.log("Checking byte length: ", checkLength);
 
-        if (!req.body.newPassword) {
-            console.log("There is no new password.")
-        } else if (checkLength > 72 || req.body.newPassword.length < 5) {
-            return res.status(400).json({ message: "Password should contain at least 5 chars." });
-        }
+        // Checking byte length of password
+        let checkLength = pwdByteLen(req.body.newPassword);
 
-        if (!req.body.oldPassword) {
-            return res.status(400).json({ message: "Your password is invalid." });
-        }
+        // check if email meets all email formatting requirements
+        let vEmail = validEmail(req.body.email);
+
+        // error catching
+        errorCatch(!vEmail, Constants.EMAIL_INVALID);
+        errorCatch(checkLength > 72 || req.body.newPassword.length < 5, Constants.PASSWORD_CHARS);
+        errorCatch(!req.body.oldPassword, Constants.PASSWORD_INVALID);
+        errorCatch(req.body.username.length < 3 || req.body.username.length > 10, Constants.USER_CHARS);
+
+        // if (!req.body.newPassword) {
+        //     console.log("There is no new password.")
+        // } else if (checkLength > 72 || req.body.newPassword.length < 5) {
+        //     return res.status(400).json({ message: Constants.PASSWORD_CHARS });
+        // }
+
+        // if (!req.body.oldPassword) {
+        //     return res.status(400).json({ message: Constants.PASSWORD_INVALID });
+        // }
+
+        // if (req.body.username.length < 3 || req.body.username.length > 10) {
+        //     return res.status(400).json({ message: Constants.USER_CHARS });
+        // }
 
         const result = await editProfileService.editProfile(req.userId, req.body.email.toString(), req.body.oldPassword.toString(), req.body.newPassword.toString());
-        res.status(result.status);
-        return res.json({ data: result.data, message: result.message });
+       
+        return res.status(result.status).json({ message: result.message });
     }
 }
 
