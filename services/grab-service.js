50a9b1e48d5e7b3656149a15874c07542c1b4011
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-11 22:27:17 +0800

    Update grab-service.js

--- services/grab-service.js
+++ services/grab-service.js
@@ -1,13 +1,16 @@
 const { Swap, Users } = require("../connect.js");
 const Constants = require("../constants/index.js");
 
-const logger = require("./service-logger/file-logger");
-const serviceName = 'grab-service';
-const serviceFn01 = 'grabBook';
+const { formatLogMsg, fileNameFormat, fnNameFormat }= require("./service-logger/log-format");
+
+const serviceName = fileNameFormat( __filename, __dirname );
+
 
 module.exports = {
 
     grabBook: async (submittedUserId, submittedSwapId) => {
+
+        let fnName = fnNameFormat(new Error());
 
         let result = {
             message: null,
@@ -20,16 +23,30 @@ module.exports = {
 
         if (!user) {
             result.message = `User ID ${submittedUserId} is not found..`;
-            // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 404;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
         };
 
         // in case submitted book does not exist in inventory OR book has just been bought by another concurrent user
         if (!book || book.availability === Constants.AVAIL_NO) {
             result.message = `Book ID ${submittedSwapId} is not found or no longer available..`;
-            // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 404;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
         };
 
@@ -37,8 +54,15 @@ module.exports = {
         if (book.price <= 0 || book.price === null || book.price === undefined) {
             // in case book price somehow is zero or lower
             result.message = `Book ID ${submittedSwapId} currently is not available for purchase..`;
-            // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 400;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
         };
 
@@ -46,16 +70,30 @@ module.exports = {
         if (user.points <= 0 || user.points === null || user.points === undefined) {
             // in case user credit somehow is zero or lower
             result.message = `User ID ${submittedUserId} currently does not have valid points..`;
-            // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 400;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
         };
 
         if (book.price > user.points) {
             // in case book price beyond user credits
             result.message = `User ID ${submittedUserId} currently does not have enough points to take book ID ${submittedSwapId}..`;
-            // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 400;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
         };
 
@@ -77,7 +115,6 @@ module.exports = {
             );
 
             console.log('updating user');
-            // logger.info(`${serviceName}-${serviceFn01}: user points successfully deducted for ${user.userId}, result ${user.points} points..`);
             // User credit MUST be deducted successfully before proceeding to "remove" book from swap availability
 
         } catch (e) {
@@ -85,8 +122,15 @@ module.exports = {
             // credit deduction unsuccessful
             console.log('User point save in swap service failed: ', e);
             result.message = `Points deduction for User ID ${submittedUserId} failed, please try again later..`;
-            // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 400;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
 
         };
@@ -111,7 +155,6 @@ module.exports = {
             );
 
             console.log('book no longer available: ', book.availability);
-            // logger.info(`${serviceName}-${serviceFn01}: Book swapId ${book.swapId} availability successfully set to ${book.availability}..`);
 
         } catch (e) {
             // when NO fails, to restore user credit, problem here, if book removal fails and somehow restore credit also fails, user loses credit for nothing, need simultaneous transaction GTH but sequelize does not have real simultaneous transactions. Ref: https://sequelize.org/master/manual/transactions.html
@@ -129,6 +172,14 @@ module.exports = {
             result.message = `Transaction not complete, please try again..`;
             // logger.error(`${serviceName}-${serviceFn01}: ${result.message}..`);
             result.status = 400;
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: result.message
+            });
+
             return result;
 
         };
@@ -136,9 +187,15 @@ module.exports = {
         result.message = `Transaction complete..`;
         result.data = user;
         result.status = 200;
-        // logger.info(`${serviceName}-${serviceFn01}: Transaction for Book swapId ${book.swapId} for user Id ${user.userId} completed successfully..`);
+        
+        formatLogMsg({
+            level: Constants.LEVEL_INFO,
+            serviceName: serviceName,
+            fnName: fnName,
+            text: result.message
+        });
+
         return result;
 
     }
-
 };
