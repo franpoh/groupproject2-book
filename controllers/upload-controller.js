fb1b228fee783b62c41dde1046aa1d306d5fdf64
CGW-G1 <91236669+CGW-G1@users.noreply.github.com>
2022-03-13 15:46:33 +0800

    Update upload-controller.js

--- controllers/upload-controller.js
+++ controllers/upload-controller.js
@@ -1,64 +1,182 @@
 const uploadService = require("../services/upload-book-service");
 
+const { formatLogMsg, fileNameFormat, controllerFnNameFormat }= require("../services/service-logger/log-format");
+
+const serviceName = fileNameFormat( __filename, __dirname );
+
 class UploadController {
     //POST /protected/uploadbook {"userid": 8, "booktitle": "Test Book Title 13", "bookauthor": "Test Book Author", "bookyear": 2007, "bookgenre": 1, "usercomments": "Test Comments Duplicate Copy of Book" }
     async uploadbook(req, res, next) {
         console.log('req.userId: ', req.userId);
         console.log("Book Details: ", req.body);
 
+        let fnName = controllerFnNameFormat();
+
+        // check for required data 
+        if (!req.userId || !req.body.userid|| !req.body.booktitle || !req.body.bookauthor) {
+            res.status(400);
+
+            let message = 'Incomplete data types submitted..'; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
+            });
+
+            return res.json({
+                message: message
+            });
+        };
+
         const loginId = req.userId;
+        const submittedUserId = parseInt(req.body.userid);
+        const submittedTitle = req.body.booktitle;
+        const submittedAuthor = req.body.bookauthor;
+        let submittedBookYear;
+        let submittedBookGenre;
+        let submittedUserComments;
+        let submittedBookCover;
 
-        if (req.userId !== req.body.userid) {
+        // check for correct type for required data 
+        if (typeof submittedUserId !== "number" || typeof submittedTitle !== "string" || typeof submittedAuthor !== "string" || !Number.isFinite(submittedUserId)) {
+            console.log("typeof userid, " + typeof submittedUserId + " typeof booktitle, " + typeof submittedTitle + " typeof bookauthor, " + typeof submittedAuthor);
             res.status(400);
+
+            let message = 'Incorrect data types submitted..'; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
+            });
+
             return res.json({
-                message: "uploadController: Incorrect User ID/Token received. Please re-login."
+                message: message
             });
         };
 
-        if (!req.body.userid || !req.body.booktitle || !req.body.bookauthor) {
+        // check for token id match with body id
+        if (loginId !== submittedUserId) {
             res.status(400);
+
+            let message = `Incorrect User ID ${submittedUserId}/Token ${loginId} received. Please re-login.`; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
+            });
+
             return res.json({
-                message: "uploadController: Incomplete Data Request provided to uploadbook."
+                message: message
             });
         };
 
-        if (
-            typeof req.body.userid !== "number" || typeof req.body.booktitle !== "string" || typeof req.body.bookauthor !== "string"
-        ) {
-            console.log("typeof userid, " + typeof req.body.userid + " typeof booktitle, " + typeof req.body.booktitle + " typeof bookauthor, " + typeof req.body.bookauthor);
+        // check for bookyear
+        if (!req.body.bookyear) {
+            submittedBookYear = null; // year to service should not be undefined, as default when empty in DB should be null
+        } else if ((req.body.bookyear).toString().length !== 4) { // What if it's a REALLY old book at year 500 AD?
             res.status(400);
-            return res.json({
-                message: "Incorrect Data Request provided to uploadbook"
+
+            let message = `Book year ${req.body.bookyear} is invald. Please provide a valid year.`; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
             });
+
+            return res.json({
+                message: message
+            });
+        } else {
+            submittedBookYear =  parseInt(req.body.bookyear);
         };
 
-        //unnecessary as front-end should have a dropdown to show genres available, but just in case.
-        if (req.body.bookgenre && (typeof req.body.bookgenre !== "string")) {
+        // check for genreId
+        if (!req.body.bookgenre){
+            submittedBookGenre = null; // genre to service should not be undefined, as default when empty in DB should be null
+        } else if (typeof req.body.bookgenre !== "number" || !Number.isFinite(parseInt(req.body.bookgenre))) {
             res.status(400);
-            return res.json({ message: "Genre does not exist." });
-        };
 
-        if (req.body.bookyear && req.body.bookyear.length < 4) {
+            let message = `Book genre ${req.body.bookgenre} is invald.`; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
+            });
+
+            return res.json({
+                message: message
+            });
+        } else {
+            submittedBookGenre = parseInt(req.body.bookgenre);
+        };
+        
+        // check for user comments
+        if (!req.body.usercomments){
+            submittedUserComments = null; // user comments to service should not be undefined, as default when empty in DB should be null
+        } else if ((req.body.usercomments).toString() <= 5) {
             res.status(400);
-            return res.json({ message: "Book year is invald. Please provide a valid year." });
+
+            let message = `User Comments is too short. Please leave a comment longer than 5 characters.`; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
+            });
+
+            return res.json({
+                message: message
+            });
+        } else {
+            submittedUserComments = (req.body.usercomments).toString();
         };
 
-        if (req.body.usercomments && req.body.usercomments.length <= 5) {
-            res.status(400);
-            return res.json({ message: "User Comments is too short. Please leave a comment longer than 5 characters." });
-        };
-
+        // check for bookcover
         function isImage(url) {
             return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
         };
 
-        if (req.body.bookcover==="" || !(isImage(req.body.bookcover))) {
+        if (!req.body.bookcover) {
+            submittedBookCover= null; // image url to service should not be undefined, as default when empty in DB should be null
+        } else if (req.body.bookcover==="" || !(isImage(req.body.bookcover))) {
             res.status(400);
-            return res.json({ message: "URL provided by user is not an image. Please provide a valid image URL."})
+
+            let message = `URL provided by user is not an image. Please provide a valid image URL.`; // need this for error formatLogMsg
+
+            formatLogMsg({
+                level: Constants.LEVEL_ERROR,
+                serviceName: serviceName,
+                fnName: fnName,
+                text: message
+            });
+
+            return res.json({
+                message: message
+            });
+        } else {
+            submittedBookCover = req.body.bookcover;
         };
 
-        const result = await uploadService.uploadbook(req.body.userid, req.body.booktitle, req.body.bookauthor, req.body.bookyear, req.body.bookgenre, req.body.usercomments, req.body.bookcover);
+        const result = await uploadService.uploadbook(submittedUserId, submittedTitle, submittedAuthor, submittedBookYear, submittedBookGenre, submittedUserComments, submittedBookCover);
         res.status(result.status);
+
+        formatLogMsg({
+            level: (result.status == 200 ? Constants.LEVEL_INFO : Constants.LEVEL_ERROR),
+            serviceName: serviceName,
+            fnName: fnName,
+            text: result.message // info only
+        });
 
         return res.json({
             data: result.data, message: result.message
